9341d337f25304fd7cb2fd4bee7db5ec8174d326
firozkabir25 <firozkabirprodhan5@gmail.com>
2026-04-15 12:00:01 +0600

    media upload and asset file update

--- @core/app/Http/Controllers/MediaUploadController.php
+++ @core/app/Http/Controllers/MediaUploadController.php
@@ -214,6 +214,52 @@ class MediaUploadController extends Controller
         return response()->json($all_image_files);
     }
 
+    // private function handle_image_upload(
+    //     $image_db,
+    //     $image,
+    //     $image_name_with_ext,
+    //     $folder_path,
+    //     $request
+    // )
+    // {
+
+    //     $image_dimension = getimagesize($image);
+    //     $image_width = $image_dimension[0];
+    //     $image_height = $image_dimension[1];
+    //     $image_dimension_for_db = $image_width . ' x ' . $image_height . ' pixels';
+    //     $image_size_for_db = $image->getSize();
+
+    //     $image_grid = 'grid-'.$image_db ;
+    //     $image_large = 'large-'. $image_db;
+    //     $image_thumb = 'thumb-'. $image_db;
+
+    //     $resize_grid_image = Image::make($image)->resize(350, null,function ($constraint) {
+    //         $constraint->aspectRatio();
+    //     });
+    //     $resize_large_image = Image::make($image)->resize(740, null,function ($constraint) {
+    //         $constraint->aspectRatio();
+    //     });
+    //     $resize_thumb_image = Image::make($image)->resize(150, 150);
+
+    //     $resize_full_image = Image::make($image)->resize($image_width, $image_height,function ($constraint) {
+    //         $constraint->aspectRatio();
+    //     });
+    //     $resize_full_image->save($folder_path .'/'. $image_db);
+        
+    //     MediaUpload::create([
+    //         'title' => $image_name_with_ext,
+    //         'size' => formatBytes($image_size_for_db),
+    //         'path' => $image_db,
+    //         'dimensions' => $image_dimension_for_db
+    //     ]);
+
+    //     if ($image_width > 150){
+    //         $resize_thumb_image->save($folder_path . $image_thumb);
+    //         $resize_grid_image->save($folder_path . $image_grid);
+    //         $resize_large_image->save($folder_path . $image_large);
+    //     }
+    // }
+
     private function handle_image_upload(
         $image_db,
         $image,
@@ -222,30 +268,42 @@ class MediaUploadController extends Controller
         $request
     )
     {
+        $image_extension = strtolower($image->extension());
+        $isGif = $image_extension === 'gif';
 
-        $image_dimension = getimagesize($image);
+        if ($isGif) {
+            // Keep the animated GIF as-is for the main uploaded file
+            $image->move($folder_path, $image_db);
+            $image_path = $folder_path . $image_db;
+        } else {
+            $image_path = $image->getRealPath();
+        }
+
+        $image_dimension = getimagesize($image_path);
         $image_width = $image_dimension[0];
         $image_height = $image_dimension[1];
         $image_dimension_for_db = $image_width . ' x ' . $image_height . ' pixels';
-        $image_size_for_db = $image->getSize();
+        $image_size_for_db = $isGif ? filesize($image_path) : $image->getSize();
 
-        $image_grid = 'grid-'.$image_db ;
-        $image_large = 'large-'. $image_db;
-        $image_thumb = 'thumb-'. $image_db;
+        $image_grid = 'grid-' . $image_db;
+        $image_large = 'large-' . $image_db;
+        $image_thumb = 'thumb-' . $image_db;
 
-        $resize_grid_image = Image::make($image)->resize(350, null,function ($constraint) {
+        $resize_grid_image = Image::make($image_path)->resize(350, null, function ($constraint) {
             $constraint->aspectRatio();
         });
-        $resize_large_image = Image::make($image)->resize(740, null,function ($constraint) {
+        $resize_large_image = Image::make($image_path)->resize(740, null, function ($constraint) {
             $constraint->aspectRatio();
         });
-        $resize_thumb_image = Image::make($image)->resize(150, 150);
+        $resize_thumb_image = Image::make($image_path)->resize(150, 150);
 
-        $resize_full_image = Image::make($image)->resize($image_width, $image_height,function ($constraint) {
-            $constraint->aspectRatio();
-        });
-        $resize_full_image->save($folder_path .'/'. $image_db);
-        
+        if (! $isGif) {
+            $resize_full_image = Image::make($image_path)->resize($image_width, $image_height, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $resize_full_image->save($folder_path . $image_db);
+        }
+
         MediaUpload::create([
             'title' => $image_name_with_ext,
             'size' => formatBytes($image_size_for_db),
@@ -253,7 +311,7 @@ class MediaUploadController extends Controller
             'dimensions' => $image_dimension_for_db
         ]);
 
-        if ($image_width > 150){
+        if ($image_width > 150) {
             $resize_thumb_image->save($folder_path . $image_thumb);
             $resize_grid_image->save($folder_path . $image_grid);
             $resize_large_image->save($folder_path . $image_large);
