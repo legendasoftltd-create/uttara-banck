bc3a68dd796a266e19adc57133562c0f0f9a6b01
firozkabir25 <firozkabirprodhan5@gmail.com>
2026-05-10 11:51:39 +0600

    Ignore env vendor and storage files

--- @core/storage/framework/views/a09c26a0a22bd1add3b29a6fae6478b9.php
+++
@@ -1,2 +0,0 @@
-<link rel="stylesheet" href="<?php echo e(asset('assets/backend/css/dropzone.css')); ?>">
-<link rel="stylesheet" href="<?php echo e(asset('assets/backend/css/media-uploader.css')); ?>"><?php /**PATH /var/www/html/ubank.example.com/@core/resources/views/components/media/css.blade.php ENDPATH**/ ?>
--- @core/storage/framework/views/c78129b63feef469537417a4d4c61b0a.php
+++
@@ -1,6 +0,0 @@
-<script>
-    $(document).on('click','#custom',function () {
-        $(this).addClass("disabled")
-        $(this).html('<i class="fas fa-spinner fa-spin mr-1"></i> <?php echo e($title); ?>');
-    });
-</script><?php /**PATH /var/www/html/ubank.example.com/@core/resources/views/components/btn/custom.blade.php ENDPATH**/ ?>
